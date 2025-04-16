6cea76597036576f211b827e832af468ff6fe764
Bastiaan Olij <mux213@gmail.com>
2025-04-16 17:51:56 +1000

    Add client matrix

--- openxr_inventory/extensions.py
+++ openxr_inventory/extensions.py
@@ -7,7 +7,9 @@ from pathlib import Path
 import re
 from typing import Dict, List, Tuple
 
-from .runtime_inventory import ExtensionEntry, RuntimeData
+from .inventory_data import ExtensionEntry
+from .runtime_inventory import RuntimeData
+from .client_inventory import ClientData
 
 _RE_IS_KHR = re.compile(r"^XR_KHR_.*")
 _RE_IS_EXT = re.compile(r"^XR_EXT_.*")
@@ -83,11 +85,17 @@ def ext_name_key(ext_name: str):
     return (categorize_ext_name(ext_name), ext_name)
 
 
-def compute_known_extensions(runtimes: List[RuntimeData]) -> List[str]:
+def compute_known_extensions(runtimes: List[RuntimeData], clients: List[ClientData]) -> List[str]:
     """Compute a list of all known extensions, sorted as in the spec itself."""
     known_extensions = set()
+
     for runtime in runtimes:
         known_extensions.update(ext.name for ext in runtime.extensions)
+
+    for client in clients:
+        for component in client.components:
+            known_extensions.update(ext.name for ext in component.extensions)
+
     return list(sorted(known_extensions, key=ext_name_key))
 
 
@@ -99,9 +107,19 @@ def compute_runtime_support(runtimes: List[RuntimeData]) -> Dict[str, List[str]]
         runtime_support[runtime.name] = support
     return runtime_support
 
+def compute_client_support(clients: List[ClientData]) -> Dict[str, List[str]]:
+    """Compute a dictionary from client names to a list of supported extension names."""
+    client_support = {}
+    for client in clients:
+        support = []
+        for component in client.components:
+            support += [ext.name for ext in component.extensions]
+        client_support[client.name] = support
+    return client_support
 
-def compute_known_form_factors(runtimes: List[RuntimeData]) -> List[str]:
+def compute_known_form_factors(runtimes: List[RuntimeData], clients: List[ClientData]) -> List[str]:
     known_form_factors = {}
+
     for runtime in runtimes:
         for ff in runtime.form_factors:
             for vc in ff.view_configurations:
@@ -109,46 +127,85 @@ def compute_known_form_factors(runtimes: List[RuntimeData]) -> List[str]:
                 for ebm in vc.environment_blend_modes:
                     known_form_factors[ff.name][vc.name] = known_form_factors[ff.name].get(vc.name, set())
                     known_form_factors[ff.name][vc.name].add(ebm.name)
+
+    for client in clients:
+        for ff in client.form_factors:
+            for vc in ff.view_configurations:
+                known_form_factors[ff.name] = known_form_factors.get(ff.name, {})
+                for ebm in vc.environment_blend_modes:
+                    known_form_factors[ff.name][vc.name] = known_form_factors[ff.name].get(vc.name, set())
+                    known_form_factors[ff.name][vc.name].add(ebm.name)
+
     return known_form_factors
 
 
-def compute_form_factor_support(runtimes: List[RuntimeData]) -> Dict[str, List[str]]:
-    runtime_form_factor_support = {}
+def compute_form_factor_support(runtimes: List[RuntimeData], clients: List[ClientData]) -> Dict[str, List[str]]:
+    form_factor_support = {}
+
     for runtime in runtimes:
         if runtime.form_factors:
-            runtime_form_factor_support[runtime.name] = {}
+            form_factor_support[runtime.name] = {}
 
             for ff in runtime.form_factors:
-                runtime_form_factor_support[runtime.name][ff.name] = {}
+                form_factor_support[runtime.name][ff.name] = {}
 
                 for vc in ff.view_configurations:
-                    runtime_form_factor_support[runtime.name][ff.name][vc.name] = set()
+                    form_factor_support[runtime.name][ff.name][vc.name] = set()
 
                     for ebm in vc.environment_blend_modes:
-                        runtime_form_factor_support[runtime.name][ff.name][vc.name].add(ebm.name)
-    return runtime_form_factor_support
+                        form_factor_support[runtime.name][ff.name][vc.name].add(ebm.name)
 
+    for client in clients:
+        if client.form_factors:
+            form_factor_support[client.name] = {}
+
+            for ff in client.form_factors:
+                form_factor_support[client.name][ff.name] = {}
+
+                for vc in ff.view_configurations:
+                    form_factor_support[client.name][ff.name][vc.name] = set()
+
+                    for ebm in vc.environment_blend_modes:
+                        form_factor_support[client.name][ff.name][vc.name].add(ebm.name)
+
+    return form_factor_support
+
+class ExtensionSupport:
+    runtime_count: int
+    client_count: int
+
+    def __init__(self, runtime_count, client_count):
+        self.runtime_count = runtime_count
+        self.client_count = client_count
 
 def compute_extension_support(
     runtimes: List[RuntimeData],
-) -> Dict[str, List[Tuple[RuntimeData, ExtensionEntry]]]:
+    clients: List[ClientData],
+) -> Dict[str, ExtensionSupport]:
     """
     For each extension, find all the runtimes that support it.
 
     Returns a dict with extension names as the keys, and a list
     of (RuntimeData, ExtensionEntry) tuples as the values.
     """
-    known_extensions = compute_known_extensions(runtimes)
+    known_extensions = compute_known_extensions(runtimes, clients)
     extension_support = {}
     for extension_name in known_extensions:
-        # Get all the support
-        support = [
-            (runtime, runtime.get_extension_entry(extension_name))
-            for runtime in runtimes
-        ]
+        runtime_count = 0
+        client_count = 0
+
+        # Count our runtime support
+        for runtime in runtimes:
+            if runtime.get_extension_entry(extension_name):
+                runtime_count += 1
+
+        # Get all the client support
+        for client in clients:
+            if client.get_extension_entry(extension_name):
+                client_count += 1
+
         # Filter out the empty ones
-        support = [(runtime, entry) for runtime, entry in support if entry]
-        extension_support[extension_name] = support
+        extension_support[extension_name] = ExtensionSupport(runtime_count, client_count)
     return extension_support
 
 
@@ -157,6 +214,7 @@ _FILENAME_STEM = "extension_support"
 
 def generate_report(
     runtimes: List[RuntimeData],
+    clients: List[ClientData],
     template_filename: str = _FILENAME_STEM + ".jinja2.html",
     out_filename: str = "public/" + _FILENAME_STEM + ".html",
 ):
@@ -173,12 +231,14 @@ def generate_report(
     template = env.get_template(template_filename)
     spec_url = "https://www.khronos.org/registry/OpenXR/specs/1.1/html/xrspec.html"
     contents = template.render(
-        extensions=compute_known_extensions(runtimes),
-        extension_support=compute_extension_support(runtimes),
+        extensions=compute_known_extensions(runtimes, clients),
+        extension_support=compute_extension_support(runtimes, clients),
         runtime_support=compute_runtime_support(runtimes),
-        known_form_factors=compute_known_form_factors(runtimes),
-        form_factor_support=compute_form_factor_support(runtimes),
+        client_support=compute_client_support(clients),
+        known_form_factors=compute_known_form_factors(runtimes, clients),
+        form_factor_support=compute_form_factor_support(runtimes, clients),
         runtimes=runtimes,
+        clients=clients,
         spec_url=spec_url,
     )
 
@@ -191,6 +251,8 @@ def generate_report(
 
 if __name__ == "__main__":
     from .runtime_inventory import load_all_runtimes
+    from .client_inventory import load_all_clients
 
     runtimes = load_all_runtimes()
-    generate_report(runtimes)
+    clients = load_all_clients()
+    generate_report(runtimes, clients)
