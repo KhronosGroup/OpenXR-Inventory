73633858fdd1a443f2fc5e07192f0665e1b29b1e
Bastiaan Olij <mux213@gmail.com>
2025-04-16 17:51:56 +1000

    Add client matrix

--- verify_schema.sh
+++ verify_schema.sh
@@ -5,6 +5,11 @@
 set -e
 
 for fn in runtimes/*.json; do
-    echo "Checking $fn against the schema"
-    python3 -m jsonschema schema.json -i "$fn"
+    echo "Checking $fn against the runtime schema"
+    python3 -m jsonschema runtime_schema.json -i "$fn"
+done
+
+for fn in clients/*.json; do
+    echo "Checking $fn against the client schema"
+    python3 -m jsonschema client_schema.json -i "$fn"
 done
